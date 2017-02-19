f6c65e109fd1000b98b55de0b321fbbd3019c54a
nomad23541 <chrisreading.email@gmail.com>
2017-02-19 00:38:16 -0700

    Two more utility classes for code seperation

---
+++ src/com/chrisreading/wallpaperchanger/utility/DownloadUtility.java
@@ -0,0 +1,38 @@
+package com.chrisreading.wallpaperchanger.utility;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import org.omg.CORBA.portable.InputStream;
+
+/**
+ * Downloads a file from a given url
+ */
+public class DownloadUtility {
+
+	public static File download(String fileUrl, String fileDest) throws IOException {
+		URL url = new URL(fileUrl);
+		BufferedInputStream input = new BufferedInputStream(url.openStream());
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		
+		byte[] buf = new byte[1024];
+		int n = 0;
+		while(-1 != (n = input.read(buf)))
+			output.write(buf, 0, n);
+		output.close();
+		input.close();
+		
+		byte[] response = output.toByteArray();
+		File file = new File(fileDest);
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(response);
+		fos.close();
+		
+		return file;
+	}
+	
+}
---
+++ src/com/chrisreading/wallpaperchanger/utility/FileLocations.java
@@ -0,0 +1,10 @@
+package com.chrisreading.wallpaperchanger.utility;
+
+/**
+ * Contains all file locations
+ */
+public class FileLocations {
+
+	public static final String DOWNLOAD_FOLDER = System.getenv("APPDATA") + "\\WallpaperChanger";
+	
+}
