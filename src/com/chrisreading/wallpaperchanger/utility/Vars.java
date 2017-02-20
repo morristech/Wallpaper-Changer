b2e8ab7f14cb83a7dae70a3002bf261212f47504
nomad23541 <chrisreading.email@gmail.com>
2017-02-20 01:36:26 -0700

    Renamed & added more finals

--- src/com/chrisreading/wallpaperchanger/utility/Vars.java
+++ src/com/chrisreading/wallpaperchanger/utility/Vars.java
@@ -1,10 +1,14 @@
 package com.chrisreading.wallpaperchanger.utility;
 
 /**
- * Contains all file locations
+ * Contains all final variables used in the program
  */
-public class FileLocations {
+public class Vars {
 
-	public static final String DOWNLOAD_FOLDER = System.getenv("APPDATA") + "\\WallpaperChanger";
+	public static final String DOWNLOAD_FOLDER_WINDOWS = System.getenv("APPDATA") + "/WallpaperChanger/";
+	public static final String DOWNLOAD_FOLDER_MAC = System.getProperty("user.home") + "/library/Application Support/WallpaperChanger/";
+	public static final String DOWNLOAD_FOLDER_LINUX = System.getProperty("user.home") + "/WallpaperChanger/";
+	
+	public static final String USER_AGENT = "WallpaperChanger by /u/SwagaliciousC";
 	
 }
