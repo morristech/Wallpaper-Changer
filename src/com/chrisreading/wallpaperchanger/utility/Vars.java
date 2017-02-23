aedaca3763740a67957359d2f2e17ca0a149f26f
nomad23541 <chrisreading.email@gmail.com>
2017-02-22 22:31:41 -0700

    Added APP_TITLE

--- src/com/chrisreading/wallpaperchanger/utility/Vars.java
+++ src/com/chrisreading/wallpaperchanger/utility/Vars.java
@@ -4,6 +4,8 @@ package com.chrisreading.wallpaperchanger.utility;
  * Contains all final variables used in the program
  */
 public class Vars {
+	
+	public static final String APP_TITLE = "Wallpaper Changer";
 
 	public static final String DOWNLOAD_FOLDER_WINDOWS = System.getenv("APPDATA") + "/WallpaperChanger/";
 	public static final String DOWNLOAD_FOLDER_MAC = System.getProperty("user.home") + "/library/Application Support/WallpaperChanger/";
