c3809fcfe392cd26d401482b10006ecce519a1b8
nomad23541 <chrisreading.email@gmail.com>
2017-02-24 21:57:21 -0700

    UI is working fully

--- src/com/chrisreading/wallpaperchanger/gui/controller/RootController.java
+++ src/com/chrisreading/wallpaperchanger/gui/controller/RootController.java
@@ -1,9 +1,14 @@
 package com.chrisreading.wallpaperchanger.gui.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.chrisreading.wallpaperchanger.gui.WCApplication;
+import com.chrisreading.wallpaperchanger.handler.BackgroundManager;
+import com.chrisreading.wallpaperchanger.handler.DownloadManager;
+import com.chrisreading.wallpaperchanger.handler.ImageGrabber;
 
 import javafx.beans.binding.Bindings;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -42,7 +47,22 @@ public class RootController {
 	
 	@FXML
 	public void onStart() {
-
+		new Thread() {
+			public void run() {
+				List<String> list = listView.getItems().stream().collect(Collectors.toList());
+				String[] listA = new String[list.size()];
+				listA = list.toArray(listA);
+				
+				ImageGrabber grabber = new ImageGrabber();
+				grabber.addSubreddits(listA);
+				grabber.look();
+				
+				DownloadManager dm = new DownloadManager(grabber);
+				dm.startDownloads();
+				BackgroundManager bm = new BackgroundManager(dm.getImages(), 1);
+				bm.init();	
+			}
+		}.start();
 	}
 	
 	@FXML
